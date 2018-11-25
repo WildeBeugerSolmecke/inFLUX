7ce5019f69f5788b7d549bd8932528aa742014e0
patrickp89 <patrickpreuss@netherspace.de>
2018-11-25 16:50:02 +0100

    Added a barebone configuration class

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+/// This class should contain all configuration necessary to build an app that is
+/// based on the inFLUX template. [It will get generated at a later devel stage!]
+class InFluxConfig {
+  static final MaterialColor primaryColor = Colors.blue;
+  static final String title = 'inFLUX';
+}
