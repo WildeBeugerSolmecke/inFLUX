f36adee0e5ce8b69c5d5d7a36703684af43ced80
Paul <paulschuetz.mail@gmail.com>
2018-11-25 17:52:54 +0100

    Initialized YoutubeChannel-Widget class

---
+++ lib/youtube_channel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// This Widget shows the user a view of the specified Youtube-Channel.
+class YoutubeChannelPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.blue
+    );
+  }
+}
