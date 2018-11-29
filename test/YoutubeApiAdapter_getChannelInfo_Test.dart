5f11398169c200ebcffac055828b27cb629fbe54
pschuetz <paulschuetz.mail@gmail.com>
2018-11-29 14:17:18 +0100

    Update YoutubeApiAdapter_getChannelInfo_Test.dart

--- test/YoutubeApiAdapter_getChannelInfo_Test.dart
+++ test/YoutubeApiAdapter_getChannelInfo_Test.dart
@@ -6,7 +6,7 @@ import '../lib/utility/Youtube/YoutubeApiAdapter.dart';
 void main() {
   test('fetching wbs channel info from "kanzleiwbs"', () async {
     YoutubeApiAdapter youtubeApiAdapter = new YoutubeApiAdapter();
-    YoutubeChannelInfo youtubeChannelInfo = await youtubeApiAdapter.getChannelInfo("kanzleiwbs", "AIzaSyBoht9JEZzn_-E6p3tHVsErOyL77yXko_M");
+    YoutubeChannelInfo youtubeChannelInfo = await youtubeApiAdapter.getChannelInfo("kanzleiwbs", "GOOGLE_API_KEY_HERE");
     assert(youtubeChannelInfo.title == "Kanzlei WBS");
     assert(youtubeChannelInfo.description.contains("Rechtsprechung "));
     assert(youtubeChannelInfo.thumbnailUrls.containsKey(ThumbnailSize.SMALL));
