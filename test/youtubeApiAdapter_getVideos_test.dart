7020c84ca44c37333c9982099c7ad08fc71e8046
pschuetz <paulschuetz.mail@gmail.com>
2018-11-29 14:17:50 +0100

    Update youtubeApiAdapter_getVideos_test.dart

--- test/youtubeApiAdapter_getVideos_test.dart
+++ test/youtubeApiAdapter_getVideos_test.dart
@@ -6,7 +6,7 @@ import 'package:test_api/test_api.dart';
 void main() {
   test('fetching the last 20 video from "kanzleiwbs" youtube channel', () async {
     YoutubeApiAdapter youtubeApiAdapter = new YoutubeApiAdapter();
-    List<YoutubeVideoInfo> videos = await youtubeApiAdapter.getVideos("UCb5TfGtSgvNPVPQawfCFuAw", "AIzaSyBoht9JEZzn_-E6p3tHVsErOyL77yXko_M", 20);
+    List<YoutubeVideoInfo> videos = await youtubeApiAdapter.getVideos("UCb5TfGtSgvNPVPQawfCFuAw", "GOOGLE_API_KEY_HERE", 20);
     assert(videos.length==20);
     assert(videos[0].id!=null);
     assert(videos[0].thumbnailUrls.containsKey(ThumbnailSize.SMALL));
