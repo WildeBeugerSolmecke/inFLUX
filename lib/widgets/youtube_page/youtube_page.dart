30f6574022dfd43a4ddbdd0f09b380dded32e0f0
pschuetz <paulschuetz.mail@gmail.com>
2018-12-15 19:20:58 +0100

    Hotfix: fixed path for youtube_video_list_item.dart in youtube_page.dart

--- lib/widgets/youtube_page/youtube_page.dart
+++ lib/widgets/youtube_page/youtube_page.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart';
 import 'package:influx/utility/youtube/youtube_api_adapter.dart';
 import 'package:influx/utility/youtube/model/youtube_channel_with_videos.dart';
 import 'package:influx/config.dart';
-import 'package:influx/widgets/youtube_video_list_item.dart';
+import 'package:influx/widgets/youtube_page/youtube_video_list_item.dart';
 
 class YoutubePage extends StatelessWidget {
   final String title;
